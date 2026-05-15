9e4055525fc55473a2a06d091db60e48731c8a41
yuki nakamura <y.nakamura@reigle.jp>
2026-05-15 23:25:08 +0900

    っd

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  // Subdirectory deploy: https://<owner>.github.io/tech-note
-  // Remove basePath if you configure a custom domain
-  basePath: '/tech-note',
 };
 
 export default nextConfig;
